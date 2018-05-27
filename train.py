85787d8abaffbfb2a94b7bc24b41dbec194a86ec
polonez <et.totum@gmail.com>
2018-05-27 12:11:02 +0900

    Use tf.logging, np splitted array, and refactored

--- train.py
+++ train.py
@@ -1,27 +1,12 @@
 import os
 from glob import glob
-import numpy as np
 import sklearn.preprocessing as prep
 import tensorflow as tf
+from tensorflow import logging
+import numpy as np
 import imageio
-from IPython.display import display
 from cnnvae import CNNVAE
 from tqdm import tqdm
-
-
-dataset = glob(os.path.join("./", "data", "*.jpg"))  # 202599
-
-n_samples = len(dataset)
-training_epochs = 1200
-batch_size = 128
-display_step = 10
-
-X_train_data = dataset[:n_samples]
-
-
-def get_random_block_from_data(data, batch_size):
-    start_index = np.random.randint(0, len(data) - batch_size)
-    return data[start_index:(start_index + batch_size)]
 
 
 def min_max_scale(d):
@@ -33,11 +18,22 @@ def min_max_scale(d):
     return d.reshape(train_shape)
 
 
-print('read image data...')
+dataset = glob(os.path.join("./", "data", "*.jpg"))  # 202599
+
+n_samples = len(dataset)
+X_train_data = dataset[:n_samples]
+training_epochs = 1200
+batch_size = 128
+display_step = 10
+
+
+logging.set_verbosity(logging.INFO)
+logging.info("read image data...")
 X_train = [imageio.imread(path) for path in tqdm(X_train_data)]
 X_train = min_max_scale(np.array(X_train).astype(np.float32))
+splitted_train_data = np.array_split(X_train, n_samples // batch_size)
 
-logdir = '/tmp/cnnvae/'
+logdir = '/tmp/cnnae/'
 vae = CNNVAE(lr=3e-6, logdir=logdir)
 saver = tf.train.Saver()
 
@@ -46,12 +42,13 @@ for epoch in range(training_epochs):
     total_batch = int(n_samples / batch_size)
     # Loop over all batches
     for i in range(total_batch):
-        batch_xs = get_random_block_from_data(X_train, batch_size)
+        batch_xs = splitted_train_data[
+            np.random.randint(0, len(splitted_train_data))]
         cost = vae.partial_fit(batch_xs, epoch)
         avg_cost += cost / n_samples
 
     # Display logs per epoch step
     if epoch % display_step == 0:
-        print("Epoch: {}, Cost: {:.9f}".format(epoch + 1, avg_cost))
-        saver.save(vae.sess, '{}cnnvae'.format(logdir), global_step=epoch)
+        logging.info("Epoch: {}, Cost: {:.9f}".format(epoch + 1, avg_cost))
+        saver.save(vae.sess, '{}cnnae'.format(logdir), global_step=epoch)
 # print("Total cost: " + str(vae.calc_total_cost(X_test)))
